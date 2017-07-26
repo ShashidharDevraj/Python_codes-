c6589efa10e35136834c8e3fc9574fa6c90127f0
Shashidhar <shashidhardevraj@gmail.com>
2017-07-26 16:34:01 +0530

    Update list compare.py

--- list compare.py
+++ list compare.py
@@ -1,3 +1,8 @@
+# Script to compare the string or substrings in 2 list and to append to a dictionary 
+# for example : the o/p will be as below
+# O/P : {'Maths_Algebra': ['Maths_Algebra_001', 'Maths_Algebra_002', 'Maths_Algebra_003'], 'Physics_Astrophysics': ['Physics_Astrophysics_001', 'Physics_Astrophysics_002']}
+
+
 from collections import defaultdict
 
 a = ["Maths_Algebra", "Physics_Astrophysics"]
