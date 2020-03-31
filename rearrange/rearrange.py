c69f89d92447e0f6236c0d856251864f05004467
Shashidhar <shashidhardevraj@gmail.com>
2020-03-31 17:55:56 +0530

    Script to read a text file and print the date present  at first position at the last.

--- rearrange/rearrange.py
+++ rearrange/rearrange.py
@@ -1,1 +1,19 @@
+# Script to read a text file and print the date present 
+# at first position at the last.
+
+# Input = "31.3.2020 corona is going wild"
+
+# Output = "corona is going wild 31.3.2020"
+
+with open("test.txt") as f:
+	for words in f.readlines():
+		arr = words.split()
+		# print(arr)
+		arr.append(arr[0])
+		arr.remove(arr[0])
+		print(" ".join(arr))
+
+
+
+
 
